cee49c012116789f832acfca6242f8459ce38a02
kirill <kirilltcoi@yandex.ru>
2022-05-15 18:04:58 +0700

    🎯 Удаление тегов

--- front/src/components/TagsBlock/TagsBlock.tsx
+++ front/src/components/TagsBlock/TagsBlock.tsx
@@ -1,11 +1,12 @@
 
 import { FC } from 'react'
 
-import { Button, Stack, Typography } from '@mui/material'
+import { Button, Grid, IconButton, Typography } from '@mui/material'
 
 import { ITag } from "models/ITag"
-import './TagsBlock.css'
 import Iconify from 'components/iconify';
+import './TagsBlock.css'
+import { tagAPI } from 'services/TagsService';
 
 interface ITagsBlock {
     tags?: ITag[];
@@ -15,23 +16,43 @@ interface ITagsBlock {
 }
 
 const TagsBlock: FC<ITagsBlock> = ({ tags, selected, maxWidth, onClick }) => {
+    const [removeTag] = tagAPI.useRemoveTagMutation()
+
+
+    const handleDeleteTag = (tag: ITag) => {
+        removeTag(tag)
+    }
+
     return (
-        <Stack sx={{ maxWidth }}>
+        <Grid sx={{ maxWidth }}>
             {tags
                 && tags.map(tag =>
                     <Button
                         variant={selected === tag.id ? "contained" : "text"}
                         key={tag.id}
                         className='sidebar__tag-button'
-                        startIcon={<Iconify icon='akar-icons:circle-fill' sx={{ color: tag.color }} />}
                         onClick={() => onClick(tag)}
                         fullWidth
                     >
-                        <Typography className="sidebar__tag-title">{tag.title}</Typography>
+                        <Grid container justifyContent='space-between' alignItems='center' direction='row' >
+                            <Grid item md={2} container justifyContent='center'>
+                                <Iconify icon='akar-icons:circle-fill' sx={{ color: tag.color, fontSize: 14 }} />
+                            </Grid>
+                            <Grid item md={8}>
+                                <Typography className="sidebar__tag-title">{tag.title}</Typography>
+                            </Grid>
+                            <Grid item md={selected === tag.id ? 2 : 1} container justifyContent='center'>
+                                {selected === tag.id &&
+                                    <IconButton onClick={() => handleDeleteTag(tag)} disableRipple disableFocusRipple sx={{ color: '#fff' }}>
+                                        <Iconify icon='ic:outline-clear' sx={{ fontSize: 24 }} />
+                                    </IconButton>
+                                }
+                            </Grid>
+                        </Grid>
                     </Button>
                 )
             }
-        </Stack>
+        </Grid >
     )
 }
 
